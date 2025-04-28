58a6efb8aa269d52f20085cb0559905f092b52f2
TatianaKasatkina <ttanchik11@yandex.ru>
2025-04-28 20:21:46 +0300

    first

--- pages/swag_labs.py
+++ pages/swag_labs.py
@@ -1,11 +1,28 @@
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
 from pages.base_page import BasePage
+
 
 class SwagLabs(BasePage):
 
     def exist_icon(self):
         try:
-            self.find_element(locator='div.login_logo')
+            self.find_element(locator="div.login_logo")
         except NoSuchElementException:
             return False
         return True
+
+    def exist_user_name(self):
+        try:
+            self.find_element(locator='#user-name')
+        except NoSuchElementException:
+            return False
+        return True
+
+    def exist_password_field(self):
+        try:
+            self.find_element(locator='#password')
+        except NoSuchElementException:
+            return False
+        return True
+
--- tests/test_check_swag.py
+++ tests/test_check_swag.py
@@ -1,12 +1,22 @@
-# 6. реализуйте тест кейс
-# a. в файле test_check_swag.py реализуйте следующий тест кейс:
-# i. перейти на страницу https://www.saucedemo.com/
-# ii. проверить наличие иконки
-# 7. реализуйте тест кейс
-# a. в файле test_check_swag.py реализуйте следующий тест кейс:
-# i. перейти на страницу https://www.saucedemo.com/
-# ii. проверить наличие поля имени
+
 # 8. реализуйте тест кейс
 # a. в файле test_check_swag.py реализуйте следующий тест кейс:
 # i. перейти на страницу https://www.saucedemo.com/
 # ii. проверить наличие поля пароля
+
+from pages.swag_labs import SwagLabs
+
+def test_check_swag_icon(browser):
+    swag_page = SwagLabs(browser)
+    swag_page.visit()
+    assert swag_page.exist_icon(), "Иконка не найдена на странице"
+
+def test_check_user_name(browser):
+    swag_page = SwagLabs(browser)
+    swag_page.visit()
+    assert swag_page.exist_user_name(), "Поле не найдено на странице"
+
+def test_check_password_field(browser):
+    swag_page = SwagLabs(browser)
+    swag_page.visit()
+    assert swag_page.exist_password_field(), "Поле не найдено на странице"
